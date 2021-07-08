af280bc72c85731cd0365d3bf06524b04d82479d
Eduardo Guimaraes <eduguimara@gmail.com>
2021-07-07 22:06:04 -0300

    Implement the user/profile endoint and database seed

--- src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.movieflix.services;
 
+import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.UserRepository;
 import org.slf4j.Logger;
@@ -8,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
@@ -16,7 +20,14 @@ public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private AuthService authService;
 
+    @Transactional(readOnly = true)
+    public UserDTO getProfile() {
+        User user = authService.authenticated();
+        return new UserDTO(user);
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
